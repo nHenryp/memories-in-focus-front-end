43e8f595781e334f227ba5056be40e2a38913f31
Anne <annaconda1984@yahoo.co.uk>
2024-08-04 16:06:24 +0100

    started React side finished sign in and sign up

---
+++ memories-in-focus-front-end/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './components/HomePage/HomePage'
+import NavBar from './components/NavBar/NavBar'
+import Dashboard from './components/Dashboard/Dashboard'
+import SignupForm from './components/SignupForm/SignupForm'
+import SigninForm from './components/SigninForm/SigninForm'
+
+const App = () => {
+
+  const [user, setUser] = useState(null)
+
+  return (
+    <>
+      <NavBar user={user} />
+
+      <Routes>
+        {  user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<HomePage />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path='/signin' element={<SigninForm setUser={setUser} />} />
+        </Routes>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ memories-in-focus-front-end/src/services/authService.js
@@ -0,0 +1,48 @@
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
+
+
+const signup = async (formData) => {
+
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/signup`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const json = await res.json();
+      if (json.error) {
+        throw new Error(json.error);
+      }
+      return json;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+};
+
+const signin = async (user) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      });
+      const json = await res.json();
+  
+      if (json.error) {
+        throw new Error(json.error);
+      }
+  
+      if (json.token) {
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+        return user;
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+
+  
+  export  { signup, signin };
