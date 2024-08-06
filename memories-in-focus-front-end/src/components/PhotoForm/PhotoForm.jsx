1ae0fb41dba1f62af90efde2890c5350796c1224
freyako-ga <freyako96@gmail.com>
2024-08-06 15:04:15 +0100

    Updates to PhotoForm component

--- memories-in-focus-front-end/src/components/PhotoForm/PhotoForm.jsx
+++ memories-in-focus-front-end/src/components/PhotoForm/PhotoForm.jsx
@@ -1,12 +1,10 @@
-// src/components/HootForm/HootForm.jsx
-
 import { useState } from 'react';
 
 const PhotoForm = (props) => {
   const [formData, setFormData] = useState({
     title: '',
     text: '',
-    category: 'News',
+    image: '',
   });
 
   const handleChange = (evt) => {
@@ -39,21 +37,6 @@ const PhotoForm = (props) => {
           value={formData.text}
           onChange={handleChange}
         />
-        <label htmlFor="category-input">Category</label>
-        <select
-          required
-          name="category"
-          id="category-input"
-          value={formData.category}
-          onChange={handleChange}
-        >
-          <option value="News">News</option>
-          <option value="Games">Games</option>
-          <option value="Music">Music</option>
-          <option value="Movies">Movies</option>
-          <option value="Sports">Sports</option>
-          <option value="Television">Television</option>
-        </select>
         <button type="submit">SUBMIT</button>
       </form>
     </main>
